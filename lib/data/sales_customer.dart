4a7d15e55ee13ded1b99421df972a00a69fc88d6
Admin <SMINIJU>
2021-02-17 16:37:11 +0400

    changed field name

--- lib/data/sales_customer.dart
+++ lib/data/sales_customer.dart
@@ -1,48 +1,66 @@
-class Sales_Customer_Data {
-  var ac_code;
-  var ac_name;
-  var address_one;
-  var contact_person;
-  var phone;
-  var mobile_no;
-  var teritory_name;
-  var route_name;
-  var os_amt;
-  var today_sale_ind;
-  var route_code;
+class Customer {
+  var val1 = null;
+  var val2;
+  var val3;
+  var val4;
+  var val5;
+  var val6;
+  var val7;
+  var val8;
+  var val9;
+  var val10;
+  var val11;
+  var val12;
+  var search;
+  var param1;
+  var param2;
 
-  var doc_type;
-  var doc_no;
-  var doc_date;
-  var ref_no;
-  var party_name;
-  var sale_type;
-  var ac_code_sales;
-  var salesman_code;
-  var confirmed;
-
-  Sales_Customer_Data.fromJson_Customer(Map<String, dynamic> json) {
-    ac_code = json['AC_CODE'];
-    ac_name = json['AC_NAME'];
-    address_one = json['ADDRESS_1'];
-    contact_person = json['CONTACT_PERSON'];
-    phone = json['PHONE'];
-    mobile_no = json['MOBILE_NO'];
-    teritory_name = json['TERRITORY_NAME'];
-    route_name = json['ROUTE_NAME'];
-    os_amt = json['OS_AMOUNT'];
-    today_sale_ind = json['TODAY_SALE_IND'];
-    route_code = json['ROUTE_CODE'];
+  Customer.fromJson_Customer(Map<String, dynamic> json) {
+    val2 = json['AC_CODE'];
+    val3 = json['OS_AMOUNT'];
+    val4 = json['AC_NAME'];
+    val5 = json['CONTACT_PERSON'];
+    val6 = json['ADDRESS_1'];
+    val7 = json['PHONE'];
+    val8 = json['MOBILE_NO'];
+    val9 = json['TERRITORY_NAME'];
+    val10 = json['ROUTE_NAME'];
+    val11 = json['TODAY_SALE_IND'];
+    val12 = json['ROUTE_CODE'];
+    search = json['AC_NAME'];
+    param1 = json['AC_CODE'];
+    param2 = json['AC_NAME'];
   }
-  Sales_Customer_Data.fromJson_Sales(Map<String, dynamic> json) {
-    doc_type = json['DOC_TYPE'];
-    doc_no = json['DOC_NO'];
-    doc_date = json['DOC_DATE'];
-    ref_no = json['REF_NO'];
-    party_name = json['PARTY_NAME'];
-    sale_type = json['SALE_TYPE'];
-    ac_code_sales = json['AC_CODE'];
-    salesman_code = json['SALESMAN_CODE'];
-    confirmed = json['CONFIRMED'];
+}
+
+class Sales {
+  var val1;
+  var val2;
+  var val3;
+  var val4;
+  var val5 = null;
+  var val6 = null;
+  var val7;
+  var val8;
+  var val9;
+  var val10;
+  var val11;
+  var search;
+  var param1;
+  var param2;
+
+  Sales.fromJson_Sales(Map<String, dynamic> json) {
+    val1 = json['DOC_DATE'];
+    val2 = json['DOC_NO'];
+    val3 = json['SALE_TYPE'];
+    val4 = json['REF_NO'];
+    val7 = json['DOC_TYPE'];
+    val8 = json['PARTY_NAME'];
+    val9 = json['AC_CODE'];
+    val10 = json['SALESMAN_CODE'];
+    val11 = json['CONFIRMED'];
+    search = json['DOC_NO'];
+    param1 = json['DOC_NO'];
+    param2 = json['PARTY_NAME'];
   }
 }
