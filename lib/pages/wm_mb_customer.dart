450404b05be8720dd0d05b7e4e448d3619f2f0db
Admin <SMINIJU>
2021-02-17 16:40:16 +0400

    customer list

--- lib/pages/wm_mb_customer.dart
+++ lib/pages/wm_mb_customer.dart
@@ -1,28 +1,36 @@
-import 'package:aware_van_sales/data/sales_customer.dart';
-import 'package:aware_van_sales/pages/wm_mb_LoginPage.dart';
-import 'package:aware_van_sales/wigdets/userListBuilder.dart';
-import 'package:aware_van_sales/wigdets/widgets.dart';
+import 'package:aware_van_sales/wigdets/listing_Builder.dart';
 import 'package:aware_van_sales/data/future_db.dart';
 import 'package:flutter/material.dart';
 
 class CustomerList extends StatefulWidget {
-  final int pageno;
-
-  const CustomerList({Key key, this.pageno}) : super(key: key);
   @override
   _CustomerListState createState() => _CustomerListState();
 }
 
 class _CustomerListState extends State<CustomerList> {
+  List _datas = List();
+  @override
+  void initState() {
+    customersaleslist().then((value) {
+      setState(() {
+        _datas.addAll(value);
+        list_length = 0;
+        list_length = _datas.length;
+        print(list_length.toString() + '....');
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: text("Customer List", Colors.white),
+        title: Text("Customer List", style: TextStyle(color: Colors.white)),
         elevation: .1,
         backgroundColor: Color.fromRGBO(59, 87, 110, 1.0),
       ),
-      body: ListBuilder(pageno: widget.pageno),
+      body: ListBuilderCommon(datas: _datas, toPage: '/SalesList', head: false),
     );
   }
 }
