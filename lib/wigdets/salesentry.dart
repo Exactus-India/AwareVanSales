d121af70a82a7c6b807d15b7b8aebbcc882c91cf
Admin <SMINIJU>
2021-02-17 16:41:48 +0400

    sales Entry page

--- lib/wigdets/salesentry.dart
+++ lib/wigdets/salesentry.dart
@@ -1,13 +1,10 @@
 import 'package:aware_van_sales/data/future_db.dart';
-import 'package:aware_van_sales/pages/wm_mb_sales.dart';
 import 'package:aware_van_sales/wigdets/widgets.dart';
 import 'package:flutter/material.dart';
 
-class SalesEntry extends StatefulWidget {
-  final String doc_no;
-  final int index;
+import 'listing_Builder.dart';
 
-  const SalesEntry({Key key, this.doc_no, this.index}) : super(key: key);
+class SalesEntry extends StatefulWidget {
   @override
   _SalesEntryState createState() => _SalesEntryState();
 }
@@ -31,13 +28,13 @@ class _SalesEntryState extends State<SalesEntry> {
   bool middle_view = false;
   List salesHDR = List();
   String serial_no;
-
+  List salesdetails = List();
+  List productList = List();
   @override
   void initState() {
-    if (widget.doc_no == null && sales_customer_name != null)
-      customer.text = sales_customer_name;
-    if (widget.doc_no != null)
-      getAllSalesHDR(widget.doc_no).then((value) {
+    if (gs_sales_param1 == null) customer.text = gs_sales_param2;
+    if (gs_sales_param1 != null)
+      getAllSalesHDR(gs_sales_param1).then((value) {
         setState(() {
           salesHDR.addAll(value);
           selectedtype = salesHDR[0]['SALE_TYPE'].toString();
@@ -49,6 +46,17 @@ class _SalesEntryState extends State<SalesEntry> {
           print(salesHDR[0]['PARTY_NAME'].toString());
         });
       });
+    if (gs_sales_param1 != null)
+      getAllSalesEntryDetails(gs_sales_param1).then((value) {
+        setState(() {
+          salesdetails.addAll(value);
+        });
+      });
+    getAllProductDetails().then((value) {
+      setState(() {
+        productList.addAll(value);
+      });
+    });
     super.initState();
   }
 
@@ -100,20 +108,14 @@ class _SalesEntryState extends State<SalesEntry> {
       body: new Padding(
         padding: new EdgeInsets.all(18.0),
         child: new Form(
-            // key: formKey,
             child: new SingleChildScrollView(
           child: Column(
             children: [
               SizedBox(height: 5),
               head(),
               SizedBox(height: 10),
-              if (middle_view == true)
-                middle(),
-              // bottomList(),
-              // bottom_list_head(),
-              Table(
-                children: [],
-              )
+              if (middle_view == true) middle(),
+              saleslist(),
             ],
           ),
         )),
@@ -124,10 +126,10 @@ class _SalesEntryState extends State<SalesEntry> {
   head() {
     return Container(
       child: Column(children: [
-        textField("Customer", customer, false),
+        textField("Customer", customer, false, true),
         SizedBox(height: 12),
         Row(children: <Widget>[
-          Flexible(child: textField("Doc_no", doc_no, false)),
+          Flexible(child: textField("Doc_no", doc_no, false, true)),
           if (doc_no.text.isEmpty)
             Flexible(
               child: Padding(
@@ -136,7 +138,7 @@ class _SalesEntryState extends State<SalesEntry> {
             ),
         ]),
         SizedBox(height: 10),
-        textField("Ref_no", ref_no, false),
+        textField("Ref_no", ref_no, false, ref_no.text != null ? true : false),
         SizedBox(height: 10),
         Row(children: <Widget>[
           Flexible(child: dropDown_salestype()),
@@ -147,57 +149,180 @@ class _SalesEntryState extends State<SalesEntry> {
           ),
         ]),
         SizedBox(height: 10),
-        textField("Remarks", remarks, false)
+        textField(
+            "Remarks", remarks, false, remarks.text != null ? true : false)
       ]),
     );
   }
 
   middle() {
     return Column(children: [
-      widget_textFiled1("Product", product, Icons.search, null),
+      widget_textFiled1("Product", product),
       SizedBox(height: 10),
-      textField("", product_name, false),
+      textField(
+          "", product_name, false, product_name.text != null ? true : false),
       SizedBox(height: 10),
       Row(children: <Widget>[
-        Flexible(child: textField("PUOM", puom, false)),
+        Flexible(
+            child: textField(
+                "PUOM", puom, false, puom.text != null ? true : false)),
         SizedBox(width: 10.0),
-        Flexible(child: textField("LUOM", luom, false)),
+        Flexible(
+            child: textField(
+                "LUOM", luom, false, luom.text != null ? true : false)),
       ]),
       SizedBox(height: 10),
-      textField("Quantity", qty, false),
+      textField("Quantity", qty, false, qty.text != null ? true : false),
       SizedBox(height: 10),
       Row(children: <Widget>[
-        Flexible(child: textField("Unit rate ", rate, false)),
+        Flexible(
+            child: textField(
+                "Unit rate ", rate, false, rate.text != null ? true : false)),
         SizedBox(width: 10.0),
-        Flexible(child: textField('Amount', rate, false)),
+        Flexible(
+            child: textField(
+                'Amount', amt, false, amt.text != null ? true : false)),
       ]),
       SizedBox(height: 10),
       Row(children: <Widget>[
-        Flexible(child: textField("VAT", vat, false)),
+        Flexible(
+            child:
+                textField("VAT", vat, false, vat.text != null ? true : false)),
         SizedBox(width: 10.0),
-        Flexible(child: textField("Net Amount", net_amt, false)),
+        Flexible(
+            child: textField("Net Amount", net_amt, false,
+                net_amt.text != null ? true : false)),
       ]),
     ]);
   }
 
-  // bottomList() {
-  //   return FutureBuilder(
-  //     future: getAllSalesEntryDetails(widget.doc_no),
-  //     builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
-  //       if (!snapshot.hasData) {
-  //         return Center(child: CircularProgressIndicator());
-  //       }
-  //       return ListView(
-  //         children: snapshot.data
-  //             .map((u) => ListTile(
-  //                   title: Column(
-  //                       children: <Widget>[Text(u['SERIAL_NO'].toString())]),
-  //                   onTap: () {},
-  //                 ))
-  //             .toList(),
-  //       );
-  //     },
-  //   );
-  // }
+  saleslist() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: DataTable(
+          columns: [
+            DataColumn(label: text("SERIAL NO", Colors.black), numeric: false),
+            DataColumn(label: text("PROD CODE", Colors.black), numeric: false),
+            DataColumn(label: text("PROD NAME", Colors.black), numeric: false),
+            DataColumn(label: text("PUOM", Colors.black), numeric: false),
+            DataColumn(label: text("QTY PUOM", Colors.black)),
+            DataColumn(label: text("LUOM", Colors.black), numeric: false),
+            DataColumn(label: text("QTY LUOM", Colors.black)),
+            DataColumn(label: text("AMOUNT", Colors.black)),
+            DataColumn(label: text("VAT", Colors.black)),
+            DataColumn(label: text("NET AMOUNT", Colors.black)),
+          ],
+          rows: salesdetails
+              .map(
+                (saleslog) => DataRow(cells: <DataCell>[
+                  DataCell(Text(saleslog['SERIAL_NO'].toString())),
+                  DataCell(Text(saleslog['PROD_CODE'].toString())),
+                  DataCell(Text(saleslog['PROD_NAME'].toString())),
+                  DataCell(Text(saleslog['P_UOM'].toString())),
+                  DataCell(Text(saleslog['QTY_PUOM'].toString())),
+                  DataCell(Text(saleslog['L_UOM'].toString())),
+                  DataCell(Text(saleslog['QTY_LUOM'].toString())),
+                  DataCell(Text(saleslog['AMOUNT'].toString())),
+                  DataCell(
+                      Text(saleslog['TX_COMPNT_AMT_1'].toStringAsFixed(3))),
+                  DataCell(Text(saleslog['NET_AMOUNT'].toStringAsFixed(3))),
+                ]),
+              )
+              .toList()),
+    );
+  }
 
+  widget_textFiled1(_text, _controller) {
+    return Row(
+      children: <Widget>[
+        Flexible(
+          child: TextField(
+            decoration: InputDecoration(
+                labelText: _text, border: const OutlineInputBorder()),
+            controller: _controller,
+          ),
+        ),
+        Flexible(
+            child: Padding(
+                padding: EdgeInsets.only(left: 30.0),
+                child: RaisedButton(
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) => prodlist());
+                    },
+                    child: Text('Search'))))
+      ],
+    );
+  }
+
+  List _datasForDisplay = List();
+  _searchBar() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+        decoration: InputDecoration(hintText: 'Search...'),
+        style: TextStyle(fontFamily: 'Montserrat', fontWeight: FontWeight.bold),
+        onChanged: (text) {
+          text = text.toUpperCase();
+          setState(() {
+            _datasForDisplay = productList.where((data) {
+              var search = data['PROD_NAME'].toString().toUpperCase();
+              return search.contains(text);
+            }).toList();
+          });
+        },
+      ),
+    );
+  }
+
+  prodlist() {
+    return AlertDialog(
+      title: Text('Products'),
+      content: Container(
+        height: 400.0,
+        width: 300.0,
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              _searchBar(),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: DataTable(
+                    columns: [
+                      DataColumn(
+                          label: text("SERIAL NO", Colors.black),
+                          numeric: false),
+                      DataColumn(
+                          label: text("PROD CODE", Colors.black),
+                          numeric: false),
+                      DataColumn(
+                          label: text("PROD NAME", Colors.black),
+                          numeric: false),
+                      DataColumn(
+                          label: text("PUOM", Colors.black), numeric: false),
+                    ],
+                    rows: productList
+                        .map(
+                          (product) => DataRow(cells: <DataCell>[
+                            DataCell(Text(product['PROD_NAME'].toString()),
+                                onTap: () {
+                              setState(() {
+                                product_name.text =
+                                    product['PROD_NAME'].toString();
+                              });
+                            }),
+                            DataCell(Text(product['PROD_CODE'].toString())),
+                            DataCell(Text(product['STK_PUOM'].toString())),
+                            DataCell(Text(product['P_UOM'].toString())),
+                          ]),
+                        )
+                        .toList()),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
