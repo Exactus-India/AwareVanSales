a94573cb4a7d9d5e3fd7c2a016863d7c910c597c
Admin <SMINIJU>
2021-02-17 13:06:01 +0400

    image size reduced

--- lib/wigdets/card.dart
+++ lib/wigdets/card.dart
@@ -4,14 +4,17 @@ card(text, page, color, context, image) {
   return Card(
     margin: EdgeInsets.all(8.0),
     child: InkWell(
-      onTap: () => {page!=null?
-        Navigator.push(context, MaterialPageRoute(builder: (context) => page)):null
+      onTap: () => {
+        page != null
+            ? Navigator.push(
+                context, MaterialPageRoute(builder: (context) => page))
+            : null
       },
       child: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            Image.asset('assets/icons/$image', width: 200, height: 150),
+            Image.asset('assets/icons/$image', width: 150, height: 100),
             // Icon(_icon, size: 70.0, color: color),
             Text(
               text,
