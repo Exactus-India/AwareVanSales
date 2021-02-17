ace9e15bf9da4e89c87f19c7fef22f315d26f2af
Admin <SMINIJU>
2021-02-17 11:49:34 +0400

    db modified

--- lib/data/future_db.dart
+++ lib/data/future_db.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:io';
+import 'package:aware_van_sales/data/salesDetail.dart';
 import 'package:aware_van_sales/data/sales_customer.dart';
 import 'package:aware_van_sales/pages/wm_mb_LoginPage.dart';
 import './User_data.dart';
@@ -8,6 +9,7 @@ import 'package:http/http.dart' as http;
 
 String gs_doc_type = 'DN90';
 String gs_company_code = 'BSG';
+
 Future<List> getAllUserName() async {
   var url = 'http://exactusnet.dyndns.org:4005/api/user';
   var response = await http.get(url);
@@ -42,39 +44,55 @@ Future<List> getAllSalesHDR(doc_no) async {
   return jsonData;
 }
 
-// Future<List> customerlist() async {
-//   var url = 'http://exactusnet.dyndns.org:4005/api/sales/customerList';
-//   var response = await http.get(url);
-//   var jsonBody = response.body;
-//   var jsonData = json.decode(jsonBody.substring(0));
-//   return jsonData;
-// }
-
-Future<List<Sales_Customer_Data>> customersaleslist() async {
+Future<List> getAllProductDetails() async {
   var url =
-      'http://exactusnet.dyndns.org:4005/api/sales/customerList/$gs_Route';
+      'http://exactusnet.dyndns.org:4005/api/sales/customerList/salesDN/search';
   var response = await http.get(url);
-  var datas = List<Sales_Customer_Data>();
+  var datas = List<Saleslog>();
   if (response.statusCode == 200) {
     Object datasJson = json.decode(response.body.substring(0));
     for (var dataJson in datasJson) {
-      datas.add(Sales_Customer_Data.fromJson_Customer(dataJson));
+      datas.add(Saleslog.fromJson(dataJson));
     }
   }
   return datas;
 }
 
-Future<List<Sales_Customer_Data>> sales_list(ac_code, salesman_code) async {
+Future<List<Customer>> customersaleslist() async {
   var url =
-      'http://exactusnet.dyndns.org:4005/api/sales/customerList/salesList/$gs_doc_type/$ac_code/$salesman_code';
+      'http://exactusnet.dyndns.org:4005/api/sales/customerList/$gs_Route';
   var response = await http.get(url);
-  var datas = List<Sales_Customer_Data>();
+  var datas = List<Customer>();
   if (response.statusCode == 200) {
     Object datasJson = json.decode(response.body.substring(0));
     for (var dataJson in datasJson) {
-      datas.add(Sales_Customer_Data.fromJson_Sales(dataJson));
+      datas.add(Customer.fromJson_Customer(dataJson));
     }
   }
+  datas.sort((a, b) {
+    var ab = a.val2;
+    var ba = b.val2;
+    return ab.compareTo(ba);
+  });
+  return datas;
+}
+
+Future<List<Sales>> saleslist(ac_code) async {
+  var url =
+      'http://exactusnet.dyndns.org:4005/api/sales/customerList/salesList/$gs_doc_type/$ac_code/$gs_currentUser_empid';
+  var response = await http.get(url);
+  var datas = List<Sales>();
+  if (response.statusCode == 200) {
+    Object datasJson = json.decode(response.body.substring(0));
+    for (var dataJson in datasJson) {
+      datas.add(Sales.fromJson_Sales(dataJson));
+    }
+  }
+  datas.sort((a, b) {
+    var ab = a.val2;
+    var ba = b.val2;
+    return ab.compareTo(ba);
+  });
   return datas;
 }
 
