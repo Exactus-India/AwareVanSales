d8cbef5b3f76ecbb7b7b0941089eb924468c00e4
Admin <SMINIJU>
2021-02-18 16:35:06 +0400

    unit field added

--- lib/data/salesproducts.dart
+++ lib/data/salesproducts.dart
@@ -7,6 +7,7 @@ class Productlist {
   var val6 = null;
   var val7;
   var val8;
+  var val9;
   var search;
 
   Productlist.fromJson(Map<String, dynamic> json) {
@@ -14,6 +15,7 @@ class Productlist {
     val2 = json['PROD_CODE'];
     val7 = json['STK_PUOM'];
     val8 = json['P_UOM'];
+    val9 = json['UNIT_PRICE'];
     val3 = val7.toString() + " " + val8.toString();
     search = val1;
   }
