d81cd8f3c14881002aa5f0fb5892dafb73d96c5d
Admin <SMINIJU>
2021-02-20 15:51:56 +0400

    middle portion

--- lib/wigdets/salesentry.dart
+++ lib/wigdets/salesentry.dart
@@ -1,4 +1,5 @@
 import 'package:aware_van_sales/data/future_db.dart';
+import 'package:aware_van_sales/wigdets/alert.dart';
 import 'package:aware_van_sales/wigdets/widgets.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +11,8 @@ class SalesEntry extends StatefulWidget {
 }
 
 class _SalesEntryState extends State<SalesEntry> {
+  String _message = '';
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
   TextEditingController customer = new TextEditingController();
   TextEditingController doc_no = new TextEditingController();
   TextEditingController ref_no = new TextEditingController();
@@ -18,6 +21,7 @@ class _SalesEntryState extends State<SalesEntry> {
   TextEditingController product_name = new TextEditingController();
   TextEditingController luom = new TextEditingController();
   TextEditingController puom = new TextEditingController();
+  TextEditingController bal_stk = new TextEditingController();
   TextEditingController qty = new TextEditingController();
   TextEditingController rate = new TextEditingController();
   TextEditingController amt = new TextEditingController();
@@ -27,9 +31,14 @@ class _SalesEntryState extends State<SalesEntry> {
   String selectedtype;
   bool middle_view = false;
   List salesHDR = List();
-  String serial_no;
+  var serial_no;
   List salesdetails = List();
   List productList = List();
+  var _puom;
+  var _luom;
+  var stk_puom;
+  var stk_luom;
+
   @override
   void initState() {
     if (gs_sales_param1 == null) customer.text = gs_sales_param2;
@@ -46,17 +55,17 @@ class _SalesEntryState extends State<SalesEntry> {
           print(salesHDR[0]['PARTY_NAME'].toString());
         });
       });
-    if (gs_sales_param1 != null)
-      getAllSalesEntryDetails(gs_sales_param1).then((value) {
-        setState(() {
-          salesdetails.addAll(value);
-        });
-      });
-    getAllProductDetails().then((value) {
+    if (gs_sales_param1 != null) fetch_saleseEntry();
+    getAllProduct().then((value) {
       setState(() {
         productList.addAll(value);
       });
     });
+    print(doc_no.text);
+    gs_sales_param1 != null
+        ? getSerialno(gs_sales_param1)
+            .then((value) => serial_no = value.toInt() + 1)
+        : serial_no = 1;
     super.initState();
   }
 
@@ -91,12 +100,20 @@ class _SalesEntryState extends State<SalesEntry> {
         elevation: .1,
         backgroundColor: Color.fromRGBO(59, 87, 110, 1.0),
         actions: <Widget>[
-          GestureDetector(onTap: () {}, child: Icon(Icons.save)),
+          GestureDetector(
+              onTap: () {
+                setState(() {
+                  productInsert();
+                });
+              },
+              child: Icon(Icons.save)),
           SizedBox(width: 20.0),
           GestureDetector(
               onTap: () {
                 setState(() {
                   middle_view = true;
+                  print(serial_no.toString());
+                  gs_list_index = null;
                 });
               },
               child: Icon(Icons.add)),
@@ -108,17 +125,18 @@ class _SalesEntryState extends State<SalesEntry> {
       body: new Padding(
         padding: new EdgeInsets.all(18.0),
         child: new Form(
+            key: _formkey,
             child: new SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(height: 5),
-              head(),
-              SizedBox(height: 10),
-              if (middle_view == true) middle(),
-              saleslist(),
-            ],
-          ),
-        )),
+              child: Column(
+                children: [
+                  SizedBox(height: 5),
+                  head(),
+                  SizedBox(height: 10),
+                  if (middle_view == true) middle(),
+                  saleslist(),
+                ],
+              ),
+            )),
       ),
     );
   }
@@ -143,10 +161,10 @@ class _SalesEntryState extends State<SalesEntry> {
         Row(children: <Widget>[
           Flexible(child: dropDown_salestype()),
           Flexible(
-            child: Padding(
-                padding: EdgeInsets.only(left: 30.0),
-                child: button_print(null, context)),
-          ),
+              child: IconButton(
+            icon: Icon(Icons.print),
+            onPressed: () {},
+          )),
         ]),
         SizedBox(height: 10),
         textField(
@@ -157,22 +175,24 @@ class _SalesEntryState extends State<SalesEntry> {
 
   middle() {
     return Column(children: [
-      widget_textFiled1("Product", product),
+      product_row("Product", product),
       SizedBox(height: 10),
       textField(
           "", product_name, false, product_name.text != null ? true : false),
       SizedBox(height: 10),
       Row(children: <Widget>[
-        Flexible(
-            child: textField(
-                "PUOM", puom, false, puom.text != null ? true : false)),
+        Flexible(child: textField("PUOM", puom, false, false)),
         SizedBox(width: 10.0),
-        Flexible(
-            child: textField(
-                "LUOM", luom, false, luom.text != null ? true : false)),
+        Flexible(child: textField("LUOM", luom, false, false)),
       ]),
       SizedBox(height: 10),
-      textField("Quantity", qty, false, qty.text != null ? true : false),
+      Row(children: <Widget>[
+        Flexible(
+          child: textField("Quantity", qty, false, true),
+        ),
+        SizedBox(width: 10.0),
+        Flexible(child: labelWidget(Colors.red[500], bal_stk)),
+      ]),
       SizedBox(height: 10),
       Row(children: <Widget>[
         Flexible(
@@ -232,97 +252,164 @@ class _SalesEntryState extends State<SalesEntry> {
     );
   }
 
-  widget_textFiled1(_text, _controller) {
+  product_row(_text, _controller) {
     return Row(
       children: <Widget>[
-        Flexible(
-          child: TextField(
-            decoration: InputDecoration(
-                labelText: _text, border: const OutlineInputBorder()),
-            controller: _controller,
-          ),
-        ),
+        Flexible(child: textField(_text, _controller, false, false)),
         Flexible(
             child: Padding(
                 padding: EdgeInsets.only(left: 30.0),
                 child: RaisedButton(
                     onPressed: () {
                       showDialog(
-                          context: context,
-                          builder: (BuildContext context) => prodlist());
+                              context: context,
+                              builder: (BuildContext context) => prodlist())
+                          .then((value) {
+                        setState(() {
+                          if (gs_list_index != null) {
+                            productClick();
+                          }
+                        });
+                      });
                     },
                     child: Text('Search'))))
       ],
     );
   }
 
-  List _datasForDisplay = List();
-  _searchBar() {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: TextField(
-        decoration: InputDecoration(hintText: 'Search...'),
-        style: TextStyle(fontFamily: 'Montserrat', fontWeight: FontWeight.bold),
-        onChanged: (text) {
-          text = text.toUpperCase();
-          setState(() {
-            _datasForDisplay = productList.where((data) {
-              var search = data['PROD_NAME'].toString().toUpperCase();
-              return search.contains(text);
-            }).toList();
-          });
-        },
-      ),
+  prodlist() {
+    return AlertDialog(
+        title: Text('Products'),
+        content: Container(
+            height: 400.0,
+            width: 300.0,
+            child: ListBuilderCommon(
+                datas: productList, head: false, toPage: null, popBack: true)));
+  }
+
+  button_generate(page, context) {
+    return RaisedButton(
+      color: Colors.green[400],
+      onPressed: () {
+        // Navigator.push(
+        //   context,
+        //   MaterialPageRoute(builder: (context) => page),
+        // );
+        page;
+      },
+      child: Text('Generate'),
     );
   }
 
-  prodlist() {
-    return AlertDialog(
-      title: Text('Products'),
-      content: Container(
-        height: 400.0,
-        width: 300.0,
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              _searchBar(),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: DataTable(
-                    columns: [
-                      DataColumn(
-                          label: text("SERIAL NO", Colors.black),
-                          numeric: false),
-                      DataColumn(
-                          label: text("PROD CODE", Colors.black),
-                          numeric: false),
-                      DataColumn(
-                          label: text("PROD NAME", Colors.black),
-                          numeric: false),
-                      DataColumn(
-                          label: text("PUOM", Colors.black), numeric: false),
-                    ],
-                    rows: productList
-                        .map(
-                          (product) => DataRow(cells: <DataCell>[
-                            DataCell(Text(product['PROD_NAME'].toString()),
-                                onTap: () {
-                              setState(() {
-                                product_name.text =
-                                    product['PROD_NAME'].toString();
-                              });
-                            }),
-                            DataCell(Text(product['PROD_CODE'].toString())),
-                            DataCell(Text(product['STK_PUOM'].toString())),
-                            DataCell(Text(product['P_UOM'].toString())),
-                          ]),
-                        )
-                        .toList()),
-              ),
-            ],
-          ),
-        ),
-      ),
+  textField(_text, _controller, _validate, read) {
+    bool obs = false;
+    if (_text == 'Password') obs = true;
+    return TextField(
+      readOnly: read,
+      obscureText: obs,
+      onTap: () {
+        setState(() {
+          if (_text == 'Quantity') {
+            productCalculate();
+          }
+        });
+      },
+      decoration: InputDecoration(
+          labelText: _text,
+          border: const OutlineInputBorder(),
+          contentPadding: EdgeInsets.all(10),
+          errorText: _validate ? 'Value Can\'t Be Empty' : null,
+          focusColor: Colors.blue,
+          labelStyle: TextStyle(color: Colors.black54)),
+      controller: _controller,
     );
+  }
+
+  fetch_saleseEntry() {
+    return getAllSalesEntryDetails(gs_sales_param1).then((value) {
+      setState(() {
+        salesdetails.clear();
+        salesdetails.addAll(value);
+      });
+    });
+  }
+
+  productClick() {
+    _puom = productList[gs_list_index].puom.toString();
+    _luom = productList[gs_list_index].luom.toString();
+    stk_puom = productList[gs_list_index].stk_puom.toString();
+    stk_luom = productList[gs_list_index].stk_luom.toString();
+    product.text = productList[gs_list_index].val2.toString();
+    product_name.text = productList[gs_list_index].val1.toString();
+    rate.text = productList[gs_list_index].val9.toString();
+    bal_stk.text = 'Bal: ' + stk_puom + ' ' + _puom;
+    amt.clear();
+    vat.clear();
+    net_amt.clear();
+    qty.clear();
+    puom.clear();
+    luom.clear();
+  }
+
+  productCalculate() {
+    var puoms;
+    var luoms;
+    puom.text.isNotEmpty ? puoms = int.parse(puom.text) : puoms = 0;
+    luom.text.isNotEmpty ? luoms = int.parse(luom.text) : luoms = 0;
+    var _qty = puoms + luoms;
+    qty.text = _qty.toString();
+    var _rate = double.parse(rate.text);
+    var _amt = _qty * _rate;
+    amt.text = (_qty * _rate).toString();
+    var _vat = (_rate * (5 / 100)) * _qty;
+    vat.text = _vat.toStringAsFixed(3);
+    net_amt.text = (_amt + _vat).toString();
+  }
+
+  productInsert() async {
+    if (!_formkey.currentState.validate()) {
+      String _msg = "required";
+      if (qty.text.isEmpty) {
+        return alert(this.context, 'puom or luom is ' + _msg, Colors.red);
+      }
+    } else {
+      // ---------------------Login Success--------------------------
+      setState(() {
+        _message = 'Loading....';
+      });
+      var resp = await product_insertion(
+          doc_no.text,
+          product.text,
+          product_name.text,
+          _puom,
+          puom.text,
+          _luom,
+          luom.text,
+          amt.text,
+          vat.text,
+          net_amt.text);
+      if (resp == 1) {
+        setState(() {
+          clearFields();
+          fetch_saleseEntry();
+          // Navigator.of(context).pop(true);
+          showToast('Created Succesfully');
+        });
+      } else {
+        showToast('error');
+      }
+    }
+  }
+
+  clearFields() {
+    product.clear();
+    product_name.clear();
+    rate.clear();
+    amt.clear();
+    vat.clear();
+    net_amt.clear();
+    qty.clear();
+    puom.clear();
+    luom.clear();
   }
 }
