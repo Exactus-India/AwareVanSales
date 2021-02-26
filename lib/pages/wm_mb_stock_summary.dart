330048a7f6814cab960ebed0e643ee275d9dafe2
UnnimayaExactus <unnimaya.in@exactus.net>
2021-02-26 13:07:14 +0530

    created with common datatable

--- lib/pages/wm_mb_stock_summary.dart
+++ lib/pages/wm_mb_stock_summary.dart
@@ -1,4 +1,4 @@
-import 'package:aware_van_sales/data/stock_sum_data.dart';
+import 'package:aware_van_sales/wigdets/dataTable_widget.dart';
 import 'package:flutter/material.dart';
 
 import '../data/future_db.dart';
@@ -11,38 +11,18 @@ class StockSummary extends StatefulWidget {
   _StockSummaryState createState() => _StockSummaryState();
 }
 
-List stocksummary = [
-  {
-    "PROD_CODE": 45,
-    "PROD_NAME": "hgfg",
-    "P_UOM": "pcs",
-    "QTY_PUOM": 54,
-    "L_UOM": 34
-  },
-  {
-    "PROD_CODE": 45,
-    "PROD_NAME": "hgfg",
-    "P_UOM": "pcs",
-    "QTY_PUOM": 54,
-    "L_UOM": 34
-  },
-  {
-    "PROD_CODE": 45,
-    "PROD_NAME": "hgfg",
-    "P_UOM": "pcs",
-    "QTY_PUOM": 54,
-    "L_UOM": 34
-  }
-];
-
 var now = new DateTime.now();
 String formatter = DateFormat('yMd').format(now);
 
 class _StockSummaryState extends State<StockSummary> {
   List stockreport = List();
-  // List<StockSum> stockreport = List<StockSum>();
-
-  List<DataRow> rows = [];
+  List column = [
+    "PRODUCT CODE",
+    "OPENING STOCK",
+    "QTY IN",
+    "QTY OUT",
+    "CLOSING STOCK"
+  ];
 
   @override
   void initState() {
@@ -51,7 +31,6 @@ class _StockSummaryState extends State<StockSummary> {
               setState(() {
                 stockreport.addAll(value);
                 print("size " + stockreport.length.toString());
-                _getData01(stockreport);
               });
             }));
 
@@ -89,88 +68,13 @@ class _StockSummaryState extends State<StockSummary> {
                 ),
                 text("User: " + gs_currentUser, Colors.black),
                 text("Date: " + formatter, Colors.black),
-                _getData01(stockreport),
-                SingleChildScrollView(
-                  padding: EdgeInsets.only(top: 20.0),
-                  scrollDirection: Axis.horizontal,
-                  child: DataTable(columns: [
-                    DataColumn(
-                      label: text("PRODUCT\nCODE", Colors.black),
-                    ),
-                    DataColumn(label: text("OPEN\nSTOCK", Colors.black)),
-                    DataColumn(
-                      label: text("QTY\nIN", Colors.black),
-                    ),
-                    DataColumn(
-                      label: text("QTY\nOUT", Colors.black),
-                    ),
-                    DataColumn(
-                        label: text("CLOSING\nSTOCK", Colors.black),
-                        numeric: true),
-                  ], rows: rows
-                      //  stockreport.forEach((stat) {
-                      // stat.item.forEach((row) {
-                      //   rows.add( DataRow(cells: <DataCell>[
-                      //                         DataCell(
-                      //                           Container(
-                      //                             child: Column(
-                      //                                 crossAxisAlignment:
-                      //                                     CrossAxisAlignment.start,
-                      //                                 children: [
-                      //                                   Text(row.prod_code.toString()),
-                      //                                   // Text(stocksum['PROD_CODE'].toString()),
-                      //                                   Row(
-                      //                                     mainAxisAlignment:
-                      //                                         MainAxisAlignment.spaceBetween,
-                      //                                     children: [
-                      //                                       Text(row.prod_name.toString()),
-                      //                                       Text(row.l_uom.toString()),
-                      //                                     ],
-                      //                                   )
-                      //                                 ]),
-                      //                           ),
-                      //                         ),
-                      //                         DataCell(Text(row.op_stk.toString())),
-                      //                         DataCell(Text(row.in_qty.toString())),
-                      //                         DataCell(Text(row.out_qty.toString())),
-                      //                         DataCell(Text(row.cl_stock.toString())),
-                      //                       ]),
-                      //                    );
-                      //                   });}),
-                      ),
-                ),
+                if (stockreport.isNotEmpty)
+                  WidgetdataTable(column: column, row: stockreport),
               ],
             ),
           ),
         ),
       ),
     );
-  }
-
-  Widget _getData01(List listOfData) {
-    listOfData.forEach((stat) {
-      stat.item.forEach((row) {
-        rows.add(DataRow(cells: [
-          DataCell(
-            Text(row.prod_name),
-          ),
-          DataCell(
-            Text(row.prod_name),
-          ),
-          // DataCell(
-          //   Text("${row.symbol}"),
-          // ),
-        ]));
-      });
-    });
-
-    // return DataTable(
-    //   columns: listOfData
-    //       .map((column) => DataColumn(
-    //             label: Container(),
-    //           ))
-    //       .toList(),
-    //   rows: rows,
-    // );
   }
 }
