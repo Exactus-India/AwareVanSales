8f354e23f9cf996569f729e2b177425b64e0b3b4
Admin <SMINIJU>
2021-03-01 12:24:08 +0400

    modified data fields

--- lib/data/salessum.dart
+++ lib/data/salessum.dart
@@ -19,18 +19,12 @@ class Salessum2 {
   var val2;
   var val3;
   var val4;
-  var val5;
-  var val6;
-  var val7;
 
   Salessum2.fromJson(Map<String, dynamic> json) {
     val1 = json['SR_NO'];
-    val2 = null;
-    val3 = null;
-    val4 = json['DESCRIPTION'];
-    val5 = json['COUNT_NO'];
-    val6 = json['AMOUNT'];
-    // val7 = json['sale_type'];
-    val7 = null;
+
+    val2 = json['DESCRIPTION'];
+    val3 = json['COUNT_NO'];
+    val4 = json['AMOUNT'];
   }
 }
