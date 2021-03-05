37cabf3638bd78df3722ee5ebb3ccd91e39e57ff
Admin <SMINIJU>
2021-03-05 12:48:11 +0400

    added sales returns cust page

---
+++ lib/pages/wm_mb_saleReturnCust.dart
@@ -0,0 +1,36 @@
+import 'package:aware_van_sales/wigdets/listing_Builder.dart';
+import 'package:aware_van_sales/data/future_db.dart';
+import 'package:flutter/material.dart';
+
+class SaleRetunCustomerList extends StatefulWidget {
+  @override
+  _SaleRetunCustomerListState createState() => _SaleRetunCustomerListState();
+}
+
+class _SaleRetunCustomerListState extends State<SaleRetunCustomerList> {
+  List _datas = List();
+  @override
+  void initState() {
+    customersalesReturnlist().then((value) {
+      setState(() {
+        _datas.addAll(value);
+        list_length = 0;
+        list_length = _datas.length;
+        print(list_length.toString() + '....');
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Customer List", style: TextStyle(color: Colors.white)),
+        elevation: .1,
+        backgroundColor: Color.fromRGBO(59, 87, 110, 1.0),
+      ),
+      body: ListBuilderCommon(datas: _datas, toPage: null, head: false),
+    );
+  }
+}
