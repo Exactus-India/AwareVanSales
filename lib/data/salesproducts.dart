5e181282419ba36593b8b85be29db57adb321d45
Admin <SMINIJU>
2021-03-12 13:01:32 +0400

    new class added

--- lib/data/salesproducts.dart
+++ lib/data/salesproducts.dart
@@ -30,19 +30,39 @@ class Productlist {
     luom = json['L_UOM'];
     stk_luom = json['STK_LUOM'];
   }
-  Productlist.fromJsonSR(Map<String, dynamic> json) {
+}
+
+class SR_Productlist {
+  var val1;
+  var val2;
+  var val3;
+  var val4;
+  var val5 = null;
+  var val6 = null;
+  var val7;
+  var val8;
+  var val9;
+  var amt;
+  var search;
+  var luom;
+  var puom;
+  var qty_luom;
+  var qty_puom;
+  var uppp;
+  SR_Productlist.fromJsonSR(Map<String, dynamic> json) {
     val1 = json['PROD_NAME'];
     val2 = json['PROD_CODE'];
-    val3 = json['QTY'];
-    val10 = json['P_UOM'];
-    val7 = json['AMOUNT'];
+    val7 = json['QTY'];
+    val8 = json['P_UOM'];
+    val9 = json['UNIT_PRICE'];
+    val3 = val7.toString() + " " + val8.toString();
     uppp = json['UPPP'];
-    val3 = val3.toString() + " " + val10.toString();
-    val5 = "Price " + val7.toString();
+    amt = json['AMOUNT'];
+    val4 = "Amount " + amt.toString();
     search = val1;
-    puom = val10;
-    // stk_puom = val7;
+    puom = val8;
     luom = json['L_UOM'];
-    stk_luom = json['STK_LUOM'];
+    qty_puom = json['QTY_PUOM'];
+    qty_luom = json['QTY_LUOM'];
   }
 }
