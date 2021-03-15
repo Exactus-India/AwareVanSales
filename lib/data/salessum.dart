998592db969054f8111afb7106f23d10c42a41fd
Admin <SMINIJU>
2021-03-15 10:57:06 +0400

    new field added

--- lib/data/salessum.dart
+++ lib/data/salessum.dart
@@ -19,12 +19,13 @@ class Salessum2 {
   var val2;
   var val3;
   var val4;
+  var val5;
 
   Salessum2.fromJson(Map<String, dynamic> json) {
     val1 = json['SR_NO'];
-
     val2 = json['DESCRIPTION'];
     val3 = json['COUNT_NO'];
     val4 = json['AMOUNT'];
+    val5 = json['SALE_TYPE'];
   }
 }
