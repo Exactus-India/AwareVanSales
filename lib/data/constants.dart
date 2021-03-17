2ecd7b8b55677dcdb6ddb9b9fa37042e0b2a14ea
Admin <SMINIJU>
2021-03-17 11:21:02 +0400

    pdf constants

---
+++ lib/data/constants.dart
@@ -0,0 +1,9 @@
+class Constants {
+  static const String ViewPdf = 'View PDF';
+  static const String SharePdf = 'Share PDF';
+
+  static const List<String> choices = <String>[
+    ViewPdf,
+    SharePdf,
+  ];
+}
